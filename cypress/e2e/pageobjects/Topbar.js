76492dc5ec904f9f173e8d5a49e4035bd10d0cf5
Rafael Bencosme <rafaelabencosmed@gmail.com>
2025-04-04 16:11:50 -0400

    Code Cleanup

--- cypress/e2e/pageobjects/Topbar.js
+++ cypress/e2e/pageobjects/Topbar.js
@@ -1,9 +1,6 @@
-class Sidebar {
-  selectBurgerMenu() {
+class Topbar {
+  logout() {
     cy.get("#react-burger-menu-btn").click();
-  }
-
-  selectLogout() {
     cy.get('[data-test="logout-sidebar-link"]').click({ timeout: 10000 });
   }
 
@@ -12,4 +9,4 @@ class Sidebar {
   }
 }
 
-export default Sidebar;
+export default Topbar;
--- cypress/e2e/ui.cy.js
+++ cypress/e2e/ui.cy.js
@@ -2,150 +2,134 @@ import { faker } from "@faker-js/faker";
 import { default as Saucedemo } from "./pageobjects/Saucedemo.js";
 import Login from "./pageobjects/Login.js";
 import Inventory from "./pageobjects/Inventory.js";
-import Sidebar from "./pageobjects/Sidebar.js";
+import Topbar from "./pageobjects/Topbar.js";
 import Checkout from "./pageobjects/Checkout.js";
 
 describe("saucedemos tests", () => {
+  let login;
+  let inventory;
+  let topBar;
+  let checkout;
+
   beforeEach(() => {
     const saucedemo = new Saucedemo();
     saucedemo.navigate();
+
+    login = new Login();
+    inventory = new Inventory();
+    topBar = new Topbar();
+    checkout = new Checkout();
   });
 
-  it("standard_user, should be able to login", () => {
-    const login = new Login();
-    login.signIn("standard_user", "secret_sauce");
+  describe("login tests", () => {
+    it("standard_user, should be able to login", () => {
+      login.signIn("standard_user", "secret_sauce");
 
-    const inventory = new Inventory();
-    inventory.appLogo.should("exist");
+      inventory.appLogo.should("exist");
+    });
+
+    it("locked_out_user, should have helper text", () => {
+      login.signIn("locked_out_user", "secret_sauce");
+
+      login.invalidUserHelper.should("exist");
+    });
+
+    it("problem_user, should be able to login", () => {
+      login.signIn("problem_user", "secret_sauce");
+
+      inventory.itemImage.should("exist");
+    });
+
+    it("performance_glitch_user, should be able to login", () => {
+      login.signIn("performance_glitch_user", "secret_sauce");
+
+      inventory.itemImage.should("exist");
+    });
+
+    it("error_user, should be able to login", () => {
+      login.signIn("error_user", "secret_sauce");
+
+      inventory.itemImage.should("exist");
+    });
+
+    it("visual_user, should be able to login", () => {
+      login.signIn("visual_user", "secret_sauce");
+
+      inventory.itemImage.should("exist");
+    });
+
+    it("should not login with empty credentials", () => {
+      login.selectLogin();
+
+      login.invalidUserHelper.should("exist");
+    });
+
+    it("should not login with invalid credentials", () => {
+      login.signIn("wrong user", "wrong user");
+
+      login.invalidUserHelper.should("exist");
+    });
+
+    it("should be able to logout", () => {
+      login.signIn("standard_user", "secret_sauce");
+
+      topBar.logout();
+
+      login.loginButton.should("exist");
+    });
   });
+  describe("checkout tests", () => {
+    it("should be able to complete a purchase", () => {
+      login.signIn("standard_user", "secret_sauce");
 
-  it("locked_out_user, should have helper text", () => {
-    const login = new Login();
-    login.signIn("locked_out_user", "secret_sauce");
+      inventory.selectItem4Title();
+      inventory.selectAddToCart();
 
-    login.invalidUserHelper.should("exist");
-  });
+      topBar.selectCart();
 
-  it("problem_user, should be able to login", () => {
-    const login = new Login();
-    login.signIn("problem_user", "secret_sauce");
+      checkout.selectCheckout();
+      checkout.fillPersonalDataForm(
+        faker.person.firstName(),
+        faker.person.lastName(),
+        faker.location.zipCode()
+      );
+      checkout.selectFinish();
 
-    const inventory = new Inventory();
-    inventory.itemImage.should("exist");
-  });
+      checkout.completedPurchaseConfirmation.should("exist");
+    });
 
-  it("performance_glitch_user, should be able to login", () => {
-    const login = new Login();
-    login.signIn("performance_glitch_user", "secret_sauce");
+    it("should be able to purchase multiple items", () => {
+      login.signIn("standard_user", "secret_sauce");
 
-    const inventory = new Inventory();
-    inventory.itemImage.should("exist");
-  });
+      inventory.selectAddBackpack();
+      inventory.selectAddBikeLight();
+      inventory.selectAddBoltShirt();
+      inventory.selectAddFleeceJacket();
 
-  it("error_user, should be able to login", () => {
-    const login = new Login();
-    login.signIn("error_user", "secret_sauce");
+      topBar.selectCart();
 
-    const inventory = new Inventory();
-    inventory.itemImage.should("exist");
-  });
+      checkout.selectCheckout();
+      checkout.fillPersonalDataForm(
+        faker.person.firstName(),
+        faker.person.lastName(),
+        faker.location.zipCode()
+      );
+      checkout.selectFinish();
 
-  it("visual_user, should be able to login", () => {
-    const login = new Login();
-    login.signIn("visual_user", "secret_sauce");
+      checkout.completedPurchaseConfirmation.should("exist");
+    });
 
-    const inventory = new Inventory();
-    inventory.itemImage.should("exist");
-  });
+    it("should display helper on name, lastname and zipcode", () => {
+      login.signIn("standard_user", "secret_sauce");
 
-  it("should not login with empty credentials", () => {
-    const login = new Login();
-    login.selectLogin();
+      inventory.selectAddBackpack();
 
-    login.invalidUserHelper.should("exist");
-  });
+      topBar.selectCart();
 
-  it("should not login with invalid credentials", () => {
-    const login = new Login();
-    login.signIn("wrong user", "wrong user");
+      checkout.selectCheckout();
+      checkout.selectContinue();
 
-    login.invalidUserHelper.should("exist");
-  });
-
-  it("should be able to logout", () => {
-    const login = new Login();
-    login.signIn("standard_user", "secret_sauce");
-
-    const sidebar = new Sidebar();
-    sidebar.selectBurgerMenu();
-    sidebar.selectLogout();
-
-    login.loginButton.should("exist");
-  });
-
-  it("should be able to complete a purchase", () => {
-    const login = new Login();
-    login.signIn("standard_user", "secret_sauce");
-
-    const inventory = new Inventory();
-    inventory.selectItem4Title();
-    inventory.selectAddToCart();
-
-    const sidebar = new Sidebar();
-    sidebar.selectCart();
-
-    const checkout = new Checkout();
-    checkout.selectCheckout();
-    checkout.fillPersonalDataForm(
-      faker.person.firstName(),
-      faker.person.lastName(),
-      faker.location.zipCode()
-    );
-    checkout.selectFinish();
-
-    checkout.completedPurchaseConfirmation.should("exist");
-  });
-
-  it("should be able to purchase multiple items", () => {
-    const login = new Login();
-    login.signIn("standard_user", "secret_sauce");
-
-    const inventory = new Inventory();
-    inventory.selectAddBackpack();
-    inventory.selectAddBikeLight();
-    inventory.selectAddBoltShirt();
-    inventory.selectAddFleeceJacket();
-
-    const sidebar = new Sidebar();
-    sidebar.selectCart();
-
-    const checkout = new Checkout();
-    checkout.selectCheckout();
-    checkout.fillPersonalDataForm(
-      faker.person.firstName(),
-      faker.person.lastName(),
-      faker.location.zipCode()
-    );
-
-    checkout.selectFinish();
-
-    checkout.completedPurchaseConfirmation.should("exist");
-  });
-
-  it("should display helper on name, lastname and zipcode", () => {
-    const login = new Login();
-    login.signIn("standard_user", "secret_sauce");
-
-    const inventory = new Inventory();
-    inventory.selectAddBackpack();
-
-    const sidebar = new Sidebar();
-    sidebar.selectCart();
-
-    const checkout = new Checkout();
-    checkout.selectCheckout();
-    checkout.selectContinue();
-
-    checkout.invalidFormDataHelper.should("exist");
+      checkout.invalidFormDataHelper.should("exist");
+    });
   });
 });
